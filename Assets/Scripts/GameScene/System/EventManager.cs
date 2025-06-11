06d23f285d1de5efec7f6ab8b5cbc03c64d698da
salmonBowl <kcfunakoshi@gmail.com>
2025-06-11 19:45:25 +0900

    fixed class TextTypingDisp

---
+++ Assets/Scripts/GameScene/System/EventManager.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class EventManager
+{
+    public enum Status
+    {
+        Title,
+        Game,
+        GameClear,
+        GamePerfectClear,
+        ThankYouForPlaying
+    }
+    public Status CurrentPhase = Status.Title;
+
+    public event Action OnGameplayStart;
+    public event Action OnRetry;
+    public event Action OnGameClear;
+    public event Action OnPerfectClear;
+    public event Action OnGameCredit;
+}
---
+++ Assets/Scripts/GameScene/System/Interface.cs
@@ -0,0 +1,5 @@
+
+interface IUpdatable
+{
+    void Update();
+}
