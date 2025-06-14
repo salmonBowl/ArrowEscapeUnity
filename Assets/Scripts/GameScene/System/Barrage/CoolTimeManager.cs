69ae9d8605d5eabc5c87aaf6303964ba547e0fb2
salmonBowl <kcfunakoshi@gmail.com>
2025-06-14 23:16:02 +0900

    create class CoolTimeManager

---
+++ Assets/Scripts/GameScene/System/Barrage/CoolTimeManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CoolTimeManager
+{
+    readonly Dictionary<CoolTimeID, float> coolTimes = new();
+
+    public CoolTimeManager()
+    {
+        foreach (CoolTimeID id in Enum.GetValues(typeof(CoolTimeID)))
+        {
+            coolTimes.Add(id, 0f);
+        }
+    }
+
+    public void Tick(float deltaTime)
+    {
+        foreach (var key in coolTimes.Keys.ToList())
+        {
+            coolTimes[key] = Mathf.Max(0, coolTimes[key] - deltaTime);
+        }
+    }
+
+    public bool IsReady(CoolTimeID id)
+    {
+        return coolTimes[id] <= 0f;
+    }
+
+    public void Reset(CoolTimeID id, float time)
+    {
+        coolTimes[id] = time;
+    }
+}
